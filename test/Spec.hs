7ffd96e905cf66b4d6ac253d0b7d727b4fa39ff5
Victor Albertsson <victor.albertsson@protonmail.com>
2021-10-03 12:08:43 +0200

    Implemented simple testing suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
+import Program.Common.Data
+
+import Control.Exception
+import Data.List
+
+typeIndex = 5 -- Number of `IntrinsicType`s (in `Program.Common.Data`)
+funcIndex = 4 -- Number of `IntrinsicFunc`s (in `Program.Common.Data`)
+
+tests = zip [1..]
+    [ -- #ObfuscatedHaskell
+      -- Compares `funcIndex` to the length of `lookupTable`
+      (flip assert
+          <*> ((==) $ fromIntegral $ length lookupTable)
+          $   funcIndex
+      ) == funcIndex
+    ]
+
+testStatus True  = "PASSED"
+testStatus False = "FAILED"
+
+testMessage (n, t) = "[TEST #" ++ show n ++ "] " ++ testStatus t
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn $ concat $ intersperse "\n" $ map testMessage tests
