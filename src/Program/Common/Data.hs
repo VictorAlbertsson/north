2da0d6ac2ef018acaab76daf691f7bd31bfe155e
Victor Albertsson <victor.albertsson@protonmail.com>
2021-10-08 22:34:01 +0200

    Finished initial version of assembly generation

--- src/Program/Common/Data.hs
+++ src/Program/Common/Data.hs
@@ -11,47 +11,17 @@ data Mode
 -- TODO: Add float/double token
 -- TODO: Add stack literal token
 data Token
-    -- Labels control type relevant behaviour, for more information see README
-    = TokenLabel           String
-    | TokenSymbol          String
-    | TokenString          String
-    | TokenUnsignedInteger Word
-    | TokenSignedInteger   Int
-    | TokenDouble          Double
-    | TokenStack           [Token]
+    = TokenSymbol   String           -- DONE
+    | TokenString   String           -- DONE
+    -- Numbers {
+    | TokenNatural  Word             -- DONE
+    | TokenInteger  Int              -- TODO
+    | TokenRational (Word, Word)     -- TODO
+    | TokenReal     Double           -- TODO
+    | TokenComplex  (Double, Double) -- TODO
+    -- }
+    | TokenStack    [Token]          -- TODO
     deriving (Show, Read, Eq)
 
-data IntrinsicType
-    -- Numeric types
-    -- TODO: Add irrational and transcendental numbers as numeric types
-    -- NOTE: Above types might be better implemented as `properties` or `typeclasses`
-    -- TODO: Implement subtyping of numeric types
-    = TypeN64 -- 64 bit natural numbers
-    | TypeZ64 -- 64 bit integer numbers
-    | TypeQ64 -- 64 bit fractional/quotient numbers
-    | TypeR64 -- 64 bit real numbers
-    | TypeC64 -- 64 bit complex numbers
-    -- Memory types
-    | TypePtr -- Memory Pointer
-    deriving (Show, Read, Eq)
-
--- TODO: Procedure table, <procedure name> <input types> <output types>
-data IntrinsicFunc
-    = FuncAdd
-    | FuncSub
-    | FuncMul
-    | FuncDiv
-    | FuncMod
-    | FuncPow
-    deriving (Show, Read, Eq)
-
-lookupTable :: [(IntrinsicFunc, [IntrinsicType], [IntrinsicType])]
-lookupTable =
-    -- NOTE: Numeric functions
-    [ (FuncAdd, [TypeN64, TypeN64], [TypeN64])
-    , (FuncSub, [TypeN64, TypeN64], [TypeZ64])
-    , (FuncMul, [TypeN64, TypeN64], [TypeN64])
-    , (FuncDiv, [TypeN64, TypeN64], [TypeQ64])
-    , (FuncMod, [TypeN64, TypeN64], [TypeN64])
-    , (FuncPow, [TypeN64, TypeN64], [TypeN64])
-    ]
+-- NOTE: Some musings on subtyping data vs. properties (typeclasses)
+-- data(N) <: data(Z) -> prop(N) :> prop(Z)
